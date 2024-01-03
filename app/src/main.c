73ff46c057b66f97e2a91279a54ba6d31b519242
Matt Wood <matt.wood@corintech.com>
2024-01-03 21:43:22 +0000

    Add support for gc9a01a display and modify GPIO configuration
    The changes include adding remote amrithvenkat12 and gc9a01a_zephyr_driver to the west.yml, and enabling the display in the prj.conf file. In addition, multiple changes were made to GPIO configuration in the m5_dial-pinctrl.dtsi file to support the new display, accompanied by some adjustments in UART console setup and touch sensor settings captured in the m5_dial.dts file. Finally, the main.c includes the display logic.

--- app/src/main.c
+++ app/src/main.c
@@ -7,18 +7,36 @@
 #include <app_version.h>
 
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/display.h>
+
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 int main(void)
 {
+    const struct device *display_dev;
+    struct display_capabilities capabilities;
 	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
 
-	while (1) {
+    do {
+        display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+        if (!device_is_ready(display_dev)) {
+            LOG_ERR("Device %s not found. Aborting sample.",
+                    display_dev->name);
+            break;
+        }
 
-		printk("Hello!\n");
+        LOG_INF("Display sample for %s", display_dev->name);
+        display_get_capabilities(display_dev, &capabilities);
+        LOG_INF("Display resolution %dx%d, orientation: %d", capabilities.x_resolution, capabilities.y_resolution,
+                capabilities.current_orientation);
 
-		k_sleep(K_MSEC(1000));
-	}
+        while (1) {
+
+            printk("Hello!\n");
+
+            k_sleep(K_MSEC(1000));
+        }
+    } while(false);
 
 	return 0;
 }
